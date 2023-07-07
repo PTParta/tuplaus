9a28c95d15cb05519ecfca34001332d801d15dae
Pauli Partanen <pauli.tapio.partanen@outlook.com>
2023-07-07 14:22:25 +0300

    Varoitusten siivousta

--- src/main/java/tuplaus/Pelaaja/Pelaaja.java
+++ src/main/java/tuplaus/Pelaaja/Pelaaja.java
@@ -4,17 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import tuplaus.Pelitapahtuma.Pelitapahtuma;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
